4a5bdf5532dccf5fc92d9e0cc2a4f4aefd199d77
Jason Spiller <jason@thetargetlab.com>
2018-09-10 12:44:42 -0600

    Reverse string done.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+const reverse = (str) => {
+  
+  // // SOLUTION 1
+  // var arrString = str.split('');
+  // var strReverseString = '';
+
+  // for (var i = arrString.length - 1; i >= 0; i--) {
+  //   strReverseString += arrString[i];
+  // }
+
+  // return strReverseString;
+
+  // // SOLUTION 2
+  // return str.split('').reverse().join('');
+
+  // // SOLUTION 3
+  // let strReverseString = '';
+
+  // for (let character of str) {
+  //   strReverseString = character + strReverseString;
+  // }
+
+  // return strReverseString;
+
+  // SOLUTION 4
+  debugger;
+  return str.split('').reduce((reverse, char) => char + reverse, '');
+}
+
+console.log(reverse('apple'));
 
 module.exports = reverse;
