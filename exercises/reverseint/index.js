76206f9727f8f3dfd7098e70733c47d9197d5916
Jason Spiller <jason@thetargetlab.com>
2018-09-10 15:02:32 -0600

    Reverse Int done.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+  // // SOLUTION 1
+  // if (Math.sign(n) < 0) {
+  //   return parseInt(n.toString().split('').reverse().join('')) * -1;
+  // } else {
+  //   return parseInt(n.toString().split('').reverse().join(''));
+  // }
+
+  // // SOLUTION 2
+  // return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n);
+
+  // SOLUTION 3
+  const reverseInt = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+    return parseInt(reverseInt) * Math.sign(n);
+
+}
+
+console.log(reverseInt(-15))
 
 module.exports = reverseInt;
