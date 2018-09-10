49bce4f0a1022601020ba8cccdcefb13bbeff793
Jason Spiller <jason@thetargetlab.com>
2018-09-10 17:08:23 -0600

    Anagram is done.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// const mapCharacters = (string) => {
+//   let charMap = {};
+
+//   for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap
+// }
+
+const parseString = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+const anagrams = (stringA, stringB) => {
+
+  // // SOLUTION 1
+  // const charMapA = mapCharacters(stringA),
+  //     charMapB = mapCharacters(stringB);
+
+  // if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+  //   return false;
+  // }
+
+  // for (let char in charMapA) {
+  //   if (charMapA[char] !== charMapB[char]) {
+  //     return false;
+  //   }
+  // }
+
+  // return true;
+
+
+  // SOLUTION 2
+  return parseString(stringA) === parseString(stringB);
+
+}
 
 module.exports = anagrams;
