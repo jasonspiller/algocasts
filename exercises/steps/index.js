d41cde238a0e904b1e09cf21df91faf76d5ad2a3
Jason Spiller <jason@thetargetlab.com>
2018-09-11 12:44:15 -0600

    Steps is done.

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+
+  // // SOLUTION 1
+  // for (let i = 0; i < n; i++) {
+    
+  //   let strStep = '';
+
+  //   for (let j = 0; j < n; j++) {
+  //     if (i >= j) {
+  //       strStep += '#';
+  //     } else {
+  //       strStep += ' ';
+  //     }        
+  //   }
+
+  //   console.log(strStep);
+    
+  // }
+
+  // SOLUTION 2
+  
+  // base case
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair)
+}
+
+steps(10);
+
 
 module.exports = steps;
